87dbfa5a07c07045e8928c194d22647ee954a99a
Erdoo <erdoo.kuhwa@gmail.com>
2021-04-10 21:20:58 -0500

    setup files

---
+++ one.js
@@ -0,0 +1,3 @@
+const number = 39;
+
+number < 10 ? console.log('Small number!') : console.log('Large number')
---
+++ three_names.js
@@ -0,0 +1,6 @@
+const secret = 'SUPER SECRET';
+
+const jason = 'jason';
+const alice = 'alice';
+
+module.exports = { jason, alice }
